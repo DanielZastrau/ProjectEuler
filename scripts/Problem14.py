f34573b4987198f4ecd36941d38988b9903dd9fb
DanielZastrau <daniel.zastrau1@gmx.de>
2026-09-06 09:58:10 +0200

    working found Wilsons Theorem and modular arithmetic

--- scripts/Problem14.py
+++ scripts/Problem14.py
@@ -1,25 +1,45 @@
+import argparse
 import time
+import math
+import itertools as it
+import functools as ft
+import operator as op
 
-def inner_collatz(number):
-    if number % 2 == 0: return number / 2
+from typing import Iterator
+
+import commons
+
+def inner_collatz(number: int) -> int:
+    if number % 2 == 0: return number // 2
     elif number % 2 == 1: return (number * 3) + 1
 
-def outer_collatz(number):
+def outer_collatz(SL: list[int], number: int) -> int:
     sequence_length = 1
     while not number == 1:
         number = int(inner_collatz(number))
-        if number < len(SL): return sequence_length + SL[number]
+        if number < len(SL):
+            return sequence_length + SL[number]
         sequence_length = sequence_length + 1
     return sequence_length
 
-max_sl = float('-inf')
-max_int = 0
-SL = [0,1]
-for integer in range(2,1000000):
-    sl = outer_collatz(integer)
-    SL.append(sl)
-    if sl > max_sl: 
-        max_sl = sl
-        max_int = integer
+def main():
+    max_sl = float('-inf')
+    max_int = 0
+    SL: list[int] = [0,1]
+    for integer in range(2,1000000):
+        sl = outer_collatz(SL, integer)
+        SL.append(sl)
+        if sl > max_sl: 
+            max_sl = sl
+            max_int = integer
 
-print(max_int)
+    print(max_int)
+
+if __name__=='__main__':
+
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    t = time.time()
+    main()
+    print(time.time() - t)
